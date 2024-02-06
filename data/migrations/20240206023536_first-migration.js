b6b4d480f24e832aa59f58434967220dbf8ceb85
Marlon Colato <mcolato2104@gmail.com>
2024-02-05 21:45:20 -0500

    wip DB Mark up!

--- data/migrations/20240206023536_first-migration.js
+++ data/migrations/20240206023536_first-migration.js
@@ -2,14 +2,26 @@
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.up = function(knex) {
-  
+exports.up = async function(knex) {
+  await knex.schema
+  .createTable('project', table => {
+    table.increments('project_id')
+  })
+  .createTable('resource', table => {
+    table.increments('resource_id')
+  })
+  .createTable('task', table => {
+    table.increments('task_id')
+  })
 };
 
 /**
  * @param { import("knex").Knex } knex
  * @returns { Promise<void> }
  */
-exports.down = function(knex) {
-  
+exports.down = async function(knex) {
+  await knex.schema
+  .dropTableIfExists('task')
+  .dropTableIfExists('resource')
+  .dropTableIfExists('project')
 };
